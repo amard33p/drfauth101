e60b28f0a141f72c863eacb5d55b0c9c42552df1
amard33p <amardeep.rox@gmail.com>
2020-03-15 01:46:54 +0530

    basic authentication

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,11 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.conf.urls import url, include
 
 @api_view(['GET', 'POST'])
-@authentication_classes([SessionAuthentication, BasicAuthentication])
+@authentication_classes([BasicAuthentication])
 @permission_classes([IsAuthenticated])
 def home(request):
         if request.method == 'POST':
